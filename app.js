3b6c1d3e96e5380c99c8eb39f4b97a1836925932
princeorji <michaelorji8039@gmail.com>
2025-02-04 10:52:54 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// Enable Cross Origin Resource Sharing to all origins by default
+app.use(cors());
+
+// Parses application/json
+app.use(express.json());
+
+// Parses urlencoded bodies
+app.use(express.urlencoded({ extended: false }));
+
+// Error Handler Middleware
+app.use((err, req, res, next) => {
+  const { message, status } = err;
+
+  res.status(status).send({ message });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
